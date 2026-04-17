f48aded64c110bc0af387a9a89ec8a0e3b52032c
Blaine <blainenicholls@protonmail.com>
2026-04-16 21:12:03 -0400

    Revert "feat: add explicit zstd compression subcommand"
    This reverts commit fe9ae05385cc389daab0553ec08c778fed1685de.

--- src/main.rs
+++ src/main.rs
@@ -1,127 +1,47 @@
-use std::{
-    fs::{read, write},
-    io::Write,
-    path::PathBuf,
-};
-use tomo_image_converter::*;
+use std::{fs::read, path::PathBuf};
+use tomo_image_converter::{image_from_canvas, image_from_texture, image_from_thumbnail};
 
-use clap::{Parser, Subcommand};
-
-const COMPRESSION_LEVEL: i32 = 3;
-
-/// Output type of texture
-#[derive(clap::ValueEnum, Debug, Clone, Copy, Eq, PartialEq)]
-pub enum PaintType {
-    Food,
-    FacePaint,
-    Interior,
-    Exterior,
-    Good,
-    Cloth,
-}
-
-impl PaintType {
-    pub fn file_name(&self) -> &'static str {
-        match self {
-            PaintType::Food => "UgcFood",
-            PaintType::FacePaint => "UgcFacePaint",
-            PaintType::Interior => "UgcInterior",
-            PaintType::Exterior => "UgcExterior",
-            PaintType::Good => "UgcGoods",
-            PaintType::Cloth => "UgcCloth",
-        }
-    }
-
-    pub fn has_thumbnail(&self) -> bool {
-        !matches!(self, Self::FacePaint)
-    }
-
-    pub fn has_canvas(&self) -> bool {
-        true
-    }
-
-    pub fn has_texture(&self) -> bool {
-        true
-    }
-}
-
-#[derive(Subcommand, Debug)]
-enum Command {
-    /// Decode canvas or ugctex to image file.
-    Decode {
-        /// Input image e.g UgcFacePaint000.ugctex.zs
-        #[arg(short, long)]
-        input: PathBuf,
-        /// Output image e.g mycat.png
-        /// supports avif, bmp, exr, ff, gif, hdr, ico, jpeg, png, pnm, qoi, tga, tiff, and webp.
-        #[arg(short, long)]
-        output: PathBuf,
-    },
-    /// Compress file using zstd
-    Compress {
-        /// Input image
-        #[arg(short, long)]
-        input: PathBuf,
-        /// Output image
-        #[arg(short, long)]
-        output: PathBuf,
-        /// Compression level
-        #[arg(short, long, default_value_t = COMPRESSION_LEVEL)]
-        level: i32,
-    },
-}
+use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct App {
-    #[command(subcommand)]
-    command: Command,
+struct Args {
+    #[arg(short, long)]
+    input: PathBuf,
+    #[arg(short, long)]
+    output: PathBuf,
 }
 
 fn main() {
-    let App { command } = App::parse();
+    let Args { input, output } = Args::parse();
 
-    match command {
-        Command::Compress {
-            input,
-            output,
-            level,
-        } => {
-            let input_bytes = read(&input).expect("Failed to read input");
-            let compressed_bytes = zstd::encode_all(input_bytes.as_slice(), level).unwrap();
-            write(&output, compressed_bytes).expect("Failed to write output");
-        }
-        Command::Decode { input, output } => {
-            let file_name = input
-                .file_name()
-                .expect("Failed to get file name")
-                .to_string_lossy();
-            let mut file_parts = file_name.split('.').collect::<Vec<_>>();
-            let mut extension = file_parts.pop().expect("Failed to get extension");
-            let prefix = file_parts.first().cloned().expect("Failed to get prefix");
+    let file_name = input
+        .file_name()
+        .expect("Failed to get file name")
+        .to_string_lossy();
+    let mut file_parts = file_name.split('.').collect::<Vec<_>>();
+    let mut extension = file_parts.pop().expect("Failed to get extension");
+    let prefix = file_parts.first().cloned().expect("Failed to get prefix");
 
-            let input_bytes = read(&input).expect("Failed to read input");
-            let buffer: Vec<u8> = if extension == "zs" {
-                extension = file_parts.pop().expect("Failed to get extension");
-                zstd::decode_all(input_bytes.as_slice()).expect("Failed to decompress input")
+    let input_bytes = read(&input).expect("Failed to read input");
+    let buffer: Vec<u8> = if extension == "zs" {
+        extension = file_parts.pop().expect("Failed to get extension");
+        zstd::decode_all(input_bytes.as_slice()).expect("Failed to decompress input")
+    } else {
+        input_bytes
+    };
+
+    let image = match extension {
+        "canvas" => image_from_canvas(&buffer).expect("Failed to read canvas"),
+        "ugctex" => {
+            if prefix.ends_with("Thumb") {
+                image_from_thumbnail(&buffer).expect("Failed to read ugctex")
             } else {
-                input_bytes
-            };
-
-            let image = match extension {
-                "canvas" => image_from_canvas(&buffer).expect("Failed to read canvas"),
-                "ugctex" => {
-                    if prefix.ends_with("Thumb") {
-                        image_from_thumbnail(&buffer).expect("Failed to read ugctex")
-                    } else {
-                        image_from_texture(&buffer, prefix.contains("Food"))
-                            .expect("Failed to read ugctex")
-                    }
-                }
-                _ => panic!("Unsupported file type"),
-            };
-
-            image.save(output).expect("Failed to write output");
+                image_from_texture(&buffer, prefix.contains("Food")).expect("Failed to read ugctex")
+            }
         }
-    }
+        _ => panic!("Unsupported file type"),
+    };
+
+    image.save(output).expect("Failed to write output");
 }
