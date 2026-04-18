c49b7685b944ed3af3217cc36050cebfd1f28cd3
Blaine <blainenicholls@protonmail.com>
2026-04-18 10:03:30 -0400

    feat: create basic window

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::{
 use tomo_image_converter::*;
 
 use clap::{Parser, Subcommand};
+slint::include_modules!();
 
 const COMPRESSION_LEVEL: i32 = 19;
 
@@ -71,198 +72,209 @@ enum Command {
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct App {
+struct Cli {
     #[command(subcommand)]
-    command: Command,
+    command: Option<Command>,
 }
 
 fn main() {
-    let App { command } = App::parse();
+    let Cli { command } = Cli::parse();
 
-    match command {
-        Command::Compress {
-            input,
-            output,
-            compression_level: level,
-        } => {
-            let input_bytes = read(&input).expect("Failed to read input");
-            let compressed_bytes = compress(&input_bytes, level).expect("Failed to compress file");
+    if let Some(command) = command {
+        match command {
+            Command::Compress {
+                input,
+                output,
+                compression_level: level,
+            } => {
+                let input_bytes = read(&input).expect("Failed to read input");
+                let compressed_bytes =
+                    compress(&input_bytes, level).expect("Failed to compress file");
 
-            write(&output, compressed_bytes).expect("Failed to write output");
-        }
-        Command::Decode { input, output } => {
-            let file_name = input
-                .file_name()
-                .expect("Failed to get file name")
-                .to_string_lossy();
-            let mut file_parts = file_name.split('.').collect::<Vec<_>>();
-            let mut extension = file_parts.pop().expect("Failed to get extension");
-            let prefix = file_parts.first().cloned().expect("Failed to get prefix");
+                write(&output, compressed_bytes).expect("Failed to write output");
+            }
+            Command::Decode { input, output } => {
+                let file_name = input
+                    .file_name()
+                    .expect("Failed to get file name")
+                    .to_string_lossy();
+                let mut file_parts = file_name.split('.').collect::<Vec<_>>();
+                let mut extension = file_parts.pop().expect("Failed to get extension");
+                let prefix = file_parts.first().cloned().expect("Failed to get prefix");
 
-            let input_bytes = read(&input).expect("Failed to read input");
-            let buffer: Vec<u8> = if extension == "zs" {
-                extension = file_parts.pop().expect("Failed to get extension");
-                zstd::decode_all(input_bytes.as_slice()).expect("Failed to decompress input")
-            } else {
-                input_bytes
-            };
-
-            let image = match extension {
-                "canvas" => image_from_canvas(&buffer).expect("Failed to read canvas"),
-                "ugctex" => {
-                    if prefix.ends_with("Thumb") {
-                        image_from_thumbnail(&buffer).expect("Failed to read ugctex")
-                    } else {
-                        image_from_texture(&buffer, prefix.contains("Food"))
-                            .expect("Failed to read ugctex")
-                    }
-                }
-                _ => panic!("Unsupported file type"),
-            };
-
-            image.save(output).expect("Failed to write output");
-        }
-        Command::Encode {
-            input,
-            output_dir,
-            output_type,
-            number,
-            skip_compression,
-            compression_level,
-            resize_method: resize,
-        } => {
-            let image = image::open(&input).expect("Failed to read input");
-
-            let file_stem = format!("{}{number:0>3}", output_type.file_name());
-            if output_type.has_texture() {
-                let size = if output_type == PaintType::Food {
-                    FOOD_SIZE
+                let input_bytes = read(&input).expect("Failed to read input");
+                let buffer: Vec<u8> = if extension == "zs" {
+                    extension = file_parts.pop().expect("Failed to get extension");
+                    zstd::decode_all(input_bytes.as_slice()).expect("Failed to decompress input")
                 } else {
-                    TEXTURE_SIZE
+                    input_bytes
                 };
 
-                let resized = match resize {
-                    ResizeType::Preserve => {
-                        image.resize(size, size, image::imageops::FilterType::Nearest)
+                let image = match extension {
+                    "canvas" => image_from_canvas(&buffer).expect("Failed to read canvas"),
+                    "ugctex" => {
+                        if prefix.ends_with("Thumb") {
+                            image_from_thumbnail(&buffer).expect("Failed to read ugctex")
+                        } else {
+                            image_from_texture(&buffer, prefix.contains("Food"))
+                                .expect("Failed to read ugctex")
+                        }
                     }
-                    ResizeType::Fill => {
-                        image.resize_to_fill(size, size, image::imageops::FilterType::Nearest)
-                    }
-                    ResizeType::Exact => {
-                        image.resize_exact(size, size, image::imageops::FilterType::Nearest)
-                    }
+                    _ => panic!("Unsupported file type"),
                 };
 
-                let bytes = texture_from_image(&resized, output_type == PaintType::Food)
-                    .expect("Failed to encode texture");
+                image.save(output).expect("Failed to write output");
+            }
+            Command::Encode {
+                input,
+                output_dir,
+                output_type,
+                number,
+                skip_compression,
+                compression_level,
+                resize_method: resize,
+            } => {
+                let image = image::open(&input).expect("Failed to read input");
 
-                let mut texture_path = output_dir.clone();
-                texture_path.push(&file_stem);
+                let file_stem = format!("{}{number:0>3}", output_type.file_name());
+                if output_type.has_texture() {
+                    let size = if output_type == PaintType::Food {
+                        FOOD_SIZE
+                    } else {
+                        TEXTURE_SIZE
+                    };
 
-                if skip_compression {
-                    texture_path.set_extension("ugctex");
+                    let resized = match resize {
+                        ResizeType::Preserve => {
+                            image.resize(size, size, image::imageops::FilterType::Nearest)
+                        }
+                        ResizeType::Fill => {
+                            image.resize_to_fill(size, size, image::imageops::FilterType::Nearest)
+                        }
+                        ResizeType::Exact => {
+                            image.resize_exact(size, size, image::imageops::FilterType::Nearest)
+                        }
+                    };
+
+                    let bytes = texture_from_image(&resized, output_type == PaintType::Food)
+                        .expect("Failed to encode texture");
+
+                    let mut texture_path = output_dir.clone();
+                    texture_path.push(&file_stem);
+
+                    if skip_compression {
+                        texture_path.set_extension("ugctex");
+                        let mut file =
+                            std::fs::File::create(&texture_path).expect("Failed to create file");
+                        file.write_all(&bytes).expect("Failed to write texture");
+
+                        return;
+                    }
+
+                    let compressed =
+                        compress(&bytes, compression_level).expect("Failed to compress");
+
+                    texture_path.set_extension("ugctex.zs");
+
                     let mut file =
                         std::fs::File::create(&texture_path).expect("Failed to create file");
-                    file.write_all(&bytes).expect("Failed to write texture");
-
-                    return;
+                    file.write_all(&compressed)
+                        .expect("Failed to write texture");
                 }
 
-                let compressed = compress(&bytes, compression_level).expect("Failed to compress");
+                if output_type.has_canvas() {
+                    let resized = match resize {
+                        ResizeType::Preserve => image.resize(
+                            CANVAS_SIZE,
+                            CANVAS_SIZE,
+                            image::imageops::FilterType::Nearest,
+                        ),
+                        ResizeType::Fill => image.resize_to_fill(
+                            CANVAS_SIZE,
+                            CANVAS_SIZE,
+                            image::imageops::FilterType::Nearest,
+                        ),
+                        ResizeType::Exact => image.resize_exact(
+                            CANVAS_SIZE,
+                            CANVAS_SIZE,
+                            image::imageops::FilterType::Nearest,
+                        ),
+                    };
 
-                texture_path.set_extension("ugctex.zs");
+                    let bytes = canvas_from_image(&resized).expect("Failed to encode canvas");
+                    if skip_compression {
+                        let mut canvas_path = output_dir.clone();
+                        canvas_path.push(&file_stem);
+                        canvas_path.set_extension("canvas");
 
-                let mut file = std::fs::File::create(&texture_path).expect("Failed to create file");
-                file.write_all(&compressed)
-                    .expect("Failed to write texture");
-            }
+                        let mut file =
+                            std::fs::File::create(&canvas_path).expect("Failed to create file");
+                        file.write_all(&bytes).expect("Failed to write canvas");
+                        return;
+                    }
 
-            if output_type.has_canvas() {
-                let resized = match resize {
-                    ResizeType::Preserve => image.resize(
-                        CANVAS_SIZE,
-                        CANVAS_SIZE,
-                        image::imageops::FilterType::Nearest,
-                    ),
-                    ResizeType::Fill => image.resize_to_fill(
-                        CANVAS_SIZE,
-                        CANVAS_SIZE,
-                        image::imageops::FilterType::Nearest,
-                    ),
-                    ResizeType::Exact => image.resize_exact(
-                        CANVAS_SIZE,
-                        CANVAS_SIZE,
-                        image::imageops::FilterType::Nearest,
-                    ),
-                };
+                    let compressed =
+                        compress(&bytes, compression_level).expect("Failed to compress");
 
-                let bytes = canvas_from_image(&resized).expect("Failed to encode canvas");
-                if skip_compression {
                     let mut canvas_path = output_dir.clone();
                     canvas_path.push(&file_stem);
-                    canvas_path.set_extension("canvas");
+                    canvas_path.set_extension("canvas.zs");
 
                     let mut file =
                         std::fs::File::create(&canvas_path).expect("Failed to create file");
-                    file.write_all(&bytes).expect("Failed to write canvas");
-                    return;
+                    file.write_all(&compressed).expect("Failed to write canvas");
                 }
 
-                let compressed = compress(&bytes, compression_level).expect("Failed to compress");
+                if output_type.has_thumbnail() {
+                    let mut thumbnail_path = output_dir.clone();
+                    let mut stem = file_stem.clone();
+                    stem.push_str("_Thumb");
 
-                let mut canvas_path = output_dir.clone();
-                canvas_path.push(&file_stem);
-                canvas_path.set_extension("canvas.zs");
+                    thumbnail_path.push(stem);
 
-                let mut file = std::fs::File::create(&canvas_path).expect("Failed to create file");
-                file.write_all(&compressed).expect("Failed to write canvas");
-            }
+                    let resized = match resize {
+                        ResizeType::Preserve => image.resize(
+                            THUMBNAIL_SIZE,
+                            THUMBNAIL_SIZE,
+                            image::imageops::FilterType::Nearest,
+                        ),
+                        ResizeType::Fill => image.resize_to_fill(
+                            THUMBNAIL_SIZE,
+                            THUMBNAIL_SIZE,
+                            image::imageops::FilterType::Nearest,
+                        ),
+                        ResizeType::Exact => image.resize_exact(
+                            THUMBNAIL_SIZE,
+                            THUMBNAIL_SIZE,
+                            image::imageops::FilterType::Nearest,
+                        ),
+                    };
 
-            if output_type.has_thumbnail() {
-                let mut thumbnail_path = output_dir.clone();
-                let mut stem = file_stem.clone();
-                stem.push_str("_Thumb");
+                    let bytes = thumbnail_from_image(&resized).expect("Failed to encode thumbnail");
 
-                thumbnail_path.push(stem);
+                    if skip_compression {
+                        thumbnail_path.set_extension("ugctex");
+                        let mut file =
+                            std::fs::File::create(&thumbnail_path).expect("Failed to create file");
+                        file.write_all(&bytes).expect("Failed to write thumbnail");
+                        return;
+                    }
 
-                let resized = match resize {
-                    ResizeType::Preserve => image.resize(
-                        THUMBNAIL_SIZE,
-                        THUMBNAIL_SIZE,
-                        image::imageops::FilterType::Nearest,
-                    ),
-                    ResizeType::Fill => image.resize_to_fill(
-                        THUMBNAIL_SIZE,
-                        THUMBNAIL_SIZE,
-                        image::imageops::FilterType::Nearest,
-                    ),
-                    ResizeType::Exact => image.resize_exact(
-                        THUMBNAIL_SIZE,
-                        THUMBNAIL_SIZE,
-                        image::imageops::FilterType::Nearest,
-                    ),
-                };
+                    let compressed =
+                        compress(&bytes, compression_level).expect("Failed to compress");
 
-                let bytes = thumbnail_from_image(&resized).expect("Failed to encode thumbnail");
-
-                if skip_compression {
-                    thumbnail_path.set_extension("ugctex");
+                    thumbnail_path.set_extension("ugctex.zs");
                     let mut file =
                         std::fs::File::create(&thumbnail_path).expect("Failed to create file");
-                    file.write_all(&bytes).expect("Failed to write thumbnail");
-                    return;
+
+                    file.write_all(&compressed)
+                        .expect("Failed to write thumbnail");
                 }
-
-                let compressed = compress(&bytes, compression_level).expect("Failed to compress");
-
-                thumbnail_path.set_extension("ugctex.zs");
-                let mut file =
-                    std::fs::File::create(&thumbnail_path).expect("Failed to create file");
-
-                file.write_all(&compressed)
-                    .expect("Failed to write thumbnail");
             }
         }
+    } else {
+        let app = AppWindow::new().expect("Failed to create main window");
+        app.run().expect("Failed to run main window");
     }
 }
 
