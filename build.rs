cbe6b7350daec5094fa6d55f002cbc5bf95f16cc
Blaine <blainenicholls@protonmail.com>
2026-05-06 19:39:42 -0400

    chore: add app icon to windows exe

--- build.rs
+++ build.rs
@@ -1,9 +1,11 @@
+use std::env::var;
+
 fn main() {
-    slint_build::compile("ui/app-window.slint").expect("Slint build failed");
-    #[cfg(windows)]
-    {
-        embed_resource::compile("assets/icon.rc", embed_resource::NONE)
+    if var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        embed_resource::compile("resources/res.rc", embed_resource::NONE)
             .manifest_optional()
             .unwrap();
     }
+
+    slint_build::compile("ui/app-window.slint").expect("Slint build failed");
 }
