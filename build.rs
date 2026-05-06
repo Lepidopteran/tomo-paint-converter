d63882f3a9cd948614a1969f85a0a003fb9d4650
Blaine <blainenicholls@protonmail.com>
2026-05-05 21:44:07 -0400

    chore: add resource_embed'

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
 fn main() {
     slint_build::compile("ui/app-window.slint").expect("Slint build failed");
+    #[cfg(windows)]
+    {
+        embed_resource::compile("assets/icon.rc", embed_resource::NONE)
+            .manifest_optional()
+            .unwrap();
+    }
 }
